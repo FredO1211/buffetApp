f4d3f2673e1be46c02e9195d4a9536f3a0025e5a
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-01-11 19:28:08 +0100

    -initial commit

---
+++ buffetApp/src/main/java/com/github/fredo1211/buffetApp/BuffetAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.fredo1211.buffetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetAppApplication.class, args);
+	}
+
+}
---
+++ buffetApp/src/test/java/com/github/fredo1211/buffetApp/BuffetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fredo1211.buffetApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
